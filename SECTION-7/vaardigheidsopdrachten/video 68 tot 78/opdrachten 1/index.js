4b3636b15f136146b15a4e5714db638f757ff8c4
SamMayer0815 <sammayer0815@gmail.com>
2021-12-03 10:06:05 +0100

    opd 1-2
    opd 1-2

---
+++ SECTION-7/vaardigheidsopdrachten/video 68 tot 78/opdrachten 1/index.js
@@ -0,0 +1,4 @@
+const a = document.getElementsByClassName('grid-container_item-1')
+
+console.log(a)
+
---
+++ SECTION-7/vaardigheidsopdrachten/video 68 tot 78/opdrachten 2/index.js
@@ -0,0 +1,7 @@
+const a = document.getElementsByClassName('grid-container_item-1')
+const b = document.getElementsByClassName('grid-container_item-2')
+
+console.log(a)
+console.log(b)
+
+
